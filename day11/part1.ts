f68678f1217175e59f0bcfe3dc9f51b775072f95
Ronan Gaillard <ronan.gaillard@live.fr>
2021-12-11 09:32:14 +0100

    feat: day 11 solution

---
+++ day11/part1.ts
@@ -0,0 +1,46 @@
+import * as fs from 'fs';
+
+const input = fs.readFileSync([__dirname, 'input.txt'].join('/'), 'utf8');
+
+const lines = input.split('\n');
+
+const energyLevels = lines.map(line => line.split('').map(level => parseInt(level, 10)));
+
+let flashCount = 0;
+
+const energyIncrease = (x: number, y: number) => {
+  if (x >= 0 && y >= 0 && x < 10 && y < 10) {
+    energyLevels[x][y] += 1;
+
+    if (energyLevels[x][y] === 10) {
+      energyIncrease(x + 1, y);
+      energyIncrease(x - 1, y);
+      energyIncrease(x, y + 1);
+      energyIncrease(x, y - 1);
+      energyIncrease(x + 1, y + 1);
+      energyIncrease(x + 1, y - 1);
+      energyIncrease(x - 1, y - 1);
+      energyIncrease(x - 1, y + 1);
+
+      flashCount += 1;
+    }
+  }
+};
+
+for (let step = 0; step < 100; step += 1) {
+  for (let x = 0; x < 10; x += 1) {
+    for (let y = 0; y < 10; y += 1) {
+      energyIncrease(x, y);
+    }
+  }
+
+  for (let x = 0; x < 10; x += 1) {
+    for (let y = 0; y < 10; y += 1) {
+      if (energyLevels[x][y] > 9) {
+        energyLevels[x][y] = 0;
+      }
+    }
+  }
+}
+
+console.log(flashCount);
